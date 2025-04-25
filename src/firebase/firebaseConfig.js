26969080e808f380fd2d63e0aa341cffa3c9e1d3
demonknight119 <akhilkumar0024@gmail.com>
2025-04-25 20:47:06 +0530

    workspace creation and document creation done

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,25 +1,20 @@
-// Import the functions you need from the SDKs you need
+// firebase/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// Your web app's Firebase configuration
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyClM_Keva5dVLcwgunK_IrA-M8dwyAt8jk",
   authDomain: "team-space-collaboration.firebaseapp.com",
   projectId: "team-space-collaboration",
   storageBucket: "team-space-collaboration.firebasestorage.app",
   messagingSenderId: "890810631260",
-  appId: "1:890810631260:web:dc32e155385e34953a5299"
+  appId: "1:890810631260:web:dc32e155385e34953a5299",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth=getAuth();
-const db = getFirestore(app);
-  
-export { db };
-
-
-
+export const auth = getAuth();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
